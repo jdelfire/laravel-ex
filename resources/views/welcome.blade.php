0480e1cba501deaf7312808fbb7d6cd439f597e9
jdelfire <39009327+jdelfire@users.noreply.github.com>
2018-05-10 07:14:48 -0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">PRJ Tech Solutions - Laravel 5</div>
+                <div class="title">PRJ Tech Solutions - Laravel 5 test</div>
             </div>
         </div>
     </body>
